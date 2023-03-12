ef198c330b91a62562a7a75a96f415ba670ceb18
yogendra dhami <yogendradhami6@gmail.com>
2023-03-12 08:36:32 +0545

    message file is created

--- learning_management_system/learning_management_system/user_login.py
+++ learning_management_system/learning_management_system/user_login.py
@@ -1,4 +1,20 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def Register(request):
+    if request.method == 'POST':
+        username= request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # email validation
+        if User.objects.filter(email=email).exists():
+            messages.warning(request, 'Email are already Exists !')
+            return redirect('register')
+        
+        # check username
+        if User.objects.filter(username=username).exists():
+            messages.warning(request,'Username are Already exists !')
+            return redirect('register')
     return render(request, 'registration/register.html')
